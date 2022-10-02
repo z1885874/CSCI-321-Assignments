b4f907e27360b7b4f83d211e34f50bad92dcc76c
AlanG497 <z1944873@students.niu.edu>
2022-10-02 13:47:46 -0500

    added bookmodel

---
+++ Books/Books/BookModel.swift
@@ -0,0 +1,18 @@
+//
+//  BookModel.swift
+//  Books
+//
+//  Created by Alan Garcia on 10/1/22.
+//
+import Foundation
+import SwiftUI
+
+struct Book:
+{
+    var image = ""
+    var authors = ""
+    var title = ""
+    var edition = ""
+    var description = ""
+    var categories = [string]()
+}
