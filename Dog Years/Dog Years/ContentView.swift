7e1c928cc18cd79076528e9cca49badfeae0a3c6
AlanG497 <z1944873@students.niu.edu>
2022-10-13 15:04:51 -0500

    added documentation and some spacing

--- Dog Years/Dog Years/ContentView.swift
+++ Dog Years/Dog Years/ContentView.swift
@@ -3,12 +3,15 @@
 //  Dog Years
 //
 //  Created by Chris Huguelet on 10/9/22.
-//Alan Garcia 
+//Z1885874
+//Alan Garcia
+//z1944873
 
 import SwiftUI
 import UIKit
 import Foundation
-
+//app that calculates  the age of your dog, has a text field to enter an
+//integer  and prints the result
 struct ContentView: View {
     @StateObject private var viewModel = DogAgeCalculator()
     var body: some View {
@@ -19,33 +22,39 @@ struct ContentView: View {
             VStack{
                 Spacer()
                 Text("How old is your dog?")
+                    .frame(width:300, height:250,
+                           alignment: .top)
                     .font(Font.custom("RujisHandwritingFontV20-vrqZ", size: 32, relativeTo: .title))
                     .foregroundColor(.white)
                 Spacer()
                 //TextField("Enter your dog's age", text: $dogYears)
                 //    .backgroundStyle(T##style: ShapeStyle##ShapeStyle)
                 TextField("Enter your score", text: $viewModel.dogYears)
+                    .position(x:200, y:-100)
                     .textFieldStyle(.roundedBorder)
                     .keyboardType(.numbersAndPunctuation)
                     .padding()
                 Spacer()
-                Image("dog")
+                Image("dog")//displays the image file
                     .resizable()
                     .frame(width: 250.0, height: 250.0)
                     .clipShape(Circle())
                     .shadow(radius: 10)
                     .overlay(Circle().stroke(Color.white))
+                    .position(x:200, y:-50)
+                    .padding()
                 Spacer()
-                Button(action: {
+                Button(action: {//Button that when pressed calls the                            //compute function
                     viewModel.compute(operation: "log")
                 }, label: {
                    Text("Human Age")
-                })
-                .frame(width: 100, height: 50)
+                })//look of the button
+                .frame(width: 150, height: 75)
                 .background(Color.white)
-                .cornerRadius(12)
+                .cornerRadius(50)
+                .padding(.vertical, 50)
                 HStack{
-                    Text("In human years, your dog is ")
+                    Text(viewModel.resultText2)
                     Text(viewModel.resultText)
                     
                 }
--- Dog Years/Dog Years/DogYearViewModel.swift
+++ Dog Years/Dog Years/DogYearViewModel.swift
@@ -6,13 +6,20 @@
 //
 
 import Foundation
-
+//class DogAgecalculator
+//initializers for various variables
+//has a function named compute that calculates the age of the dog 
 class DogAgeCalculator: ObservableObject{
     @Published var dogYears = ""
     @Published var resultText = ""
-    
+    @Published var resultText2 = ""
     @Published var errorMessage = ""
     @Published var showAlert = false
+    //function that computes the age of the dog
+    //@param result, takes the input and finds the natural log of it
+    // times 16 plus 31
+    //displays the result of it and some text
+    //if there is no valid integer, then it displays an error message
     func compute(operation: String)
     {
         guard let number = Double(dogYears) else{
@@ -23,10 +30,10 @@ class DogAgeCalculator: ObservableObject{
         var result: Double
         switch operation {
         default:
-            result = 16*log(number)+31
+            result = (16*log(number))+31
         }
         resultText = String(format:"%.2f", result)
-        
+        resultText2 = String("In human years, your dog is ")
     }
   
 }
